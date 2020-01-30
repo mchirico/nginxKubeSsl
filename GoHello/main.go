6a8ae5cf8eec02054119b966bf5b7361d1a27041
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2020-01-30 11:21:45 -0500

    update

---
+++ GoHello/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+        "fmt"
+        "log"
+        "net/http"
+        "os"
+)
+
+func main() {
+        // use PORT environment variable, or default to 8080
+        port := "2341"
+        if fromEnv := os.Getenv("PORT"); fromEnv != "" {
+                port = fromEnv
+        }
+
+        // register hello function to handle all requests
+        server := http.NewServeMux()
+        server.HandleFunc("/", hello)
+
+        // start the web server on port and accept requests
+        log.Printf("Server listening on port %s", port)
+        err := http.ListenAndServe(":"+port, server)
+        log.Fatal(err)
+}
+
+// hello responds to the request with a plain-text "Hello, world" message.
+func hello(w http.ResponseWriter, r *http.Request) {
+        log.Printf("Serving request: %s", r.URL.Path)
+        host, _ := os.Hostname()
+        fmt.Fprintf(w, "Hello, world!  Update 2\n")
+        fmt.Fprintf(w, "Version: 2.0.0\n")
+        fmt.Fprintf(w, "Hostname: %s\n", host)
+}
+
